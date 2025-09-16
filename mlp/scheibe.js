e72666f19dc9e2a0d58cc8294fd092f0030bee84
Jannik Lorenz <dev@janniklorenz.de>
2025-09-16 20:32:06 +0200

    remove rings 1-4 for mlp

--- mlp/scheibe.js
+++ mlp/scheibe.js
@@ -9,10 +9,10 @@ module.exports = {
 		{ value:  7, width:  101.5, color: "black", text: true,  textColor: "white", zoomScale: 2, zoom: zoom.z3, hitColor: "#00bffF" },
 		{ value:  6, width:  128.0, color: "black", text: true,  textColor: "white", zoomScale: 1.5, zoom: zoom.z2, hitColor: "#00bffF" },
 		{ value:  5, width:  154.5, color: "black", text: true,  textColor: "white", zoomScale: 1.25, zoom: zoom.z2, hitColor: "#00bffF" },
-		{ value:  4, width: 181.0, color: "white", text: true,  textColor: "black", zoomScale: 1, zoom: zoom.z1, hitColor: "#00bffF" }, // Ab hier ist Spiegel weiß und eigentlich nicht mehr vorhanden
-		{ value:  3, width: 207.5, color: "white", text: true,  textColor: "black", zoomScale: 1, zoom: zoom.z1, hitColor: "#00bffF" },
-		{ value:  2, width: 234.0, color: "white", text: true,  textColor: "black", zoomScale: 1, zoom: zoom.z1, hitColor: "#00bffF" },
-		{ value:  1, width: 260.5, color: "white", text: true,  textColor: "black", zoomScale: 1, zoom: zoom.z1, hitColor: "#00bffF" },
+		// { value:  4, width: 181.0, color: "white", text: true,  textColor: "black", zoomScale: 1, zoom: zoom.z1, hitColor: "#00bffF" }, // Ab hier ist Spiegel weiß und eigentlich nicht mehr vorhanden
+		// { value:  3, width: 207.5, color: "white", text: true,  textColor: "black", zoomScale: 1, zoom: zoom.z1, hitColor: "#00bffF" },
+		// { value:  2, width: 234.0, color: "white", text: true,  textColor: "black", zoomScale: 1, zoom: zoom.z1, hitColor: "#00bffF" },
+		// { value:  1, width: 260.5, color: "white", text: true,  textColor: "black", zoomScale: 1, zoom: zoom.z1, hitColor: "#00bffF" },
 	],
 	ringeDrawOnly: [
 		{ width:  11.0, color: "black", textColor: "white" },
